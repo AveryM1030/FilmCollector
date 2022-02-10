3cf174f5cdadb2d38bcc0187c0f3f69c798bae80
Avery Miles <MilesA@chc.edu>
2022-02-10 10:41:40 -0500

    Finished View Movies and Create Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
 	# validates method specifices that an movie is valid only if
 	# a title, duration, rating release date are present
-	validates :title, :duration, :rating, :rerlease_date, presence: true
+	validates :title, :duration, :rating, :release_date, presence: true
 end
